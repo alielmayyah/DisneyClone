02cd2f3a5de37abaf6e43e022faaa43bc92ebbbb
alielmayyah <alielmayyah@gmail.com>
2024-08-14 19:01:02 +0300

    Deploy-1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Components/Header";
+import Slider from "./Components/Slider";
+import ProductionHouse from "./Components/ProductionHouse";
+import GenresMovieList from "./Components/GenresMovieList";
+function App() {
+  return (
+    <div className="">
+      <Header />
+      <Slider />
+      <ProductionHouse />
+      <GenresMovieList />
+    </div>
+  );
+}
+
+export default App;
